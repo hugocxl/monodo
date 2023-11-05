4e35a0a62ac4c98211b4701c10b023f22d0e24cd
Hugo Corta <corta.hugo@gmail.com>
2023-11-05 15:13:13 +0100

    fix: types checking in lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,6 +3,5 @@ module.exports = {
     'pnpm --reporter=silent lint:fix',
     'pnpm --reporter=silent format:fix'
   ],
-  // '{backend,frontend,shared}/**/*.{ts,tsx}': "bash -c 'pnpm types:check'"
   '{backend,frontend,shared}/**/*.{ts,tsx}': 'tsc-files --noEmit'
 }
