6b35cee2e62b5df6eddb5d122360dc93a8cec9f0
Hugo Corta <corta.hugo@gmail.com>
2023-11-11 10:44:21 +0100

    WIP

--- backend/src/modules/users/infra/http/users.router.ts
+++ backend/src/modules/users/infra/http/users.router.ts
@@ -1,7 +1,6 @@
 // Dependencies
 import express, { Router } from 'express'
 import { signInController, signUpController } from '../../application'
-import { authMiddleware } from '@/shared/infra/http/middlewares'
 
 // Types
 import type { AuthRequest } from '@/shared/infra'
@@ -11,11 +10,8 @@ const usersRouter: Router = express.Router()
 usersRouter.post('/sign-in', (req, res) => signInController.execute(req, res))
 usersRouter.post('/sign-up', (req, res) => signUpController.execute(req, res))
 usersRouter.post('/sign-out', (req: AuthRequest, res) => {
-  req.session.user = false
   return res.status(200).json({ message: 'logged out' })
 })
-usersRouter.get('/me', authMiddleware, (req: AuthRequest, res) =>
-  res.json(req.session.user)
-)
+usersRouter.get('/me', (req: AuthRequest, res) => res.json({ message: 'ok' }))
 
 export { usersRouter }
--- backend/src/shared/infra/http/api/api.ts
+++ backend/src/shared/infra/http/api/api.ts
@@ -1,11 +1,10 @@
 import express, { Router } from 'express'
 import { usersRouter } from '@/modules/users/infra'
 import { tasksRouter } from '@/modules/tasks/infra'
-import { authMiddleware } from '../middlewares'
 
 const router: Router = express.Router()
 
 router.use('/users', usersRouter)
-router.use('/tasks', authMiddleware, tasksRouter)
+router.use('/tasks', tasksRouter)
 
 export { router }
