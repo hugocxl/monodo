aad9dd21f42cea42fbc11e0b65d06461050e93df
Hugo Corta <corta.hugo@gmail.com>
2023-11-14 10:28:08 +0100

    fix: add moment to overcome safari error on dates

--- frontend/src/shared/hooks/use-selected-date/use-selected-date.hook.ts
+++ frontend/src/shared/hooks/use-selected-date/use-selected-date.hook.ts
@@ -1,21 +1,20 @@
 // Dependencies
 import { useSearchParams } from 'react-router-dom'
+import moment from 'moment'
 
 export const useSelectedDate = (): [string, (date: Date | string) => void] => {
   const [searchParams, setSearchParams] = useSearchParams()
   const paramsDate = searchParams.get('date')
   const date = paramsDate ?? getFormattedDate(new Date())
+  // const dateObj = moment(date, 'YYYY-MM-DD')
 
   function getFormattedDate(date: Date) {
-    return date.toLocaleDateString('default', {
-      day: '2-digit',
-      month: 'numeric',
-      year: 'numeric'
-    })
+    return moment(date).format('YYYY-MM-DD')
   }
 
   function onChangeDate(date: Date | string) {
     const newDate = getFormattedDate(new Date(date))
+
     const searchParams = new URLSearchParams({
       date: newDate
     })
