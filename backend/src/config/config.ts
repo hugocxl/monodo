fa14ea4c29f4af8d5e59ee57eced644d9f5357d3
hugocxl <corta.hugo@gmail.com>
2024-03-27 08:56:16 +0100

    WIP

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,34 +1,34 @@
-import * as dotenv from 'dotenv'
-import Joi from 'joi'
+import * as dotenv from "dotenv";
+import Joi from "joi";
 
-dotenv.config()
+dotenv.config();
 
 const envVarsSchema = Joi.object()
-  .keys({
-    NODE_ENV: Joi.string()
-      .valid('production', 'development', 'test')
-      .required(),
-    PORT: Joi.number().default(3000),
-    MONGODB_URL: Joi.string().required().description('Mongo DB url'),
-    DB_NAME: Joi.string().required().description('Mongo DB name'),
-    CLIENT_URL: Joi.string().required().description('Client url')
-  })
-  .unknown()
+	.keys({
+		NODE_ENV: Joi.string()
+			.valid("production", "development", "test")
+			.required(),
+		PORT: Joi.number().default(3000),
+		MONGODB_URL: Joi.string().required().description("Mongo DB url"),
+		DB_NAME: Joi.string().required().description("Mongo DB name"),
+		CLIENT_URL: Joi.string().required().description("Client url"),
+	})
+	.unknown();
 
 const { value: envVars, error } = envVarsSchema
-  .prefs({ errors: { label: 'key' } })
-  .validate(process.env)
+	.prefs({ errors: { label: "key" } })
+	.validate(process.env);
 
 if (error) {
-  throw new Error(`Config validation error: ${error.message}`)
+	throw new Error(`Config validation error: ${error.message}`);
 }
 
 export const config = {
-  env: envVars.NODE_ENV,
-  port: envVars.PORT,
-  clientUrl: envVars.CLIENT_URL,
-  mongoose: {
-    url: envVars.MONGODB_URL,
-    dbName: envVars.DB_NAME
-  }
-}
+	env: envVars.NODE_ENV,
+	port: envVars.PORT,
+	clientUrl: envVars.CLIENT_URL,
+	mongoose: {
+		url: envVars.MONGODB_URL,
+		dbName: envVars.DB_NAME,
+	},
+};
